90832f51cf7f9bef2ae2142dce0695f818425c67
Asmit Kumar <asmit.k23csai@nst.rishihood.edu.in>
2026-03-27 03:15:08 +0530

    url update

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://roommate-matcher-pcz6.onrender.com', // your Django backend base URL
+  baseURL: 'https://roommate-matcher-1.onrender.com', // your Django backend base URL
   withCredentials: true, // allows cookies / session headers
 });
 
